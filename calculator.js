c604fbfaf3eb051d5fbaf0a6c6f263ec94af1f85
Joel Velis <joelvelis94@gmail.com>
2019-01-02 14:00:02 -0500

    figured out how to add a row

--- calculator.js
+++ calculator.js
@@ -1,7 +1,23 @@
+let body = document.body;
+let tr = document.createElement('tr');
+let input = document.createElement('input');
+let table = document.getElementById('myTable')
+let row = table.insertRow();
+let td1 = row.insertCell();
+let td2 = row.insertCell();
+let td3 = row.insertCell();
+
+
 function myClick() {
-	let body = document.body;
 	let amount1 = document.getElementById('amount1').value;
-	let tr = document.createElement('tr');
 	tr.innerText = amount1 * .15;
 	body.append(tr);
+}
+
+function addItem() {
+	td1.innerHTML = "";
+	td2.innerHTML = "";
+	td3.innerHTML = "Amount2 ";
+	td3.append(input);
+
 }
